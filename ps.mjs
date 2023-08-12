9aa479fadbefb91d7e49c727d86f274bddabc7bf
mattrichmo <mattrichmo@github.com>
2023-08-12 12:27:14 -0700

    Group Scenes. groupScenes Function Added. Grouping scenes with UUID for each scene to be referenced to later

--- ps.mjs
+++ ps.mjs
@@ -125,13 +125,23 @@ const docRaw = {
     pageRaw: [],
     pageLineRaw: [[]],
     pageLineCharRaw: [[[]]],
-    sceneToPage: [[{
+    sceneRaw: [],
+    sceneToPage: [[{ // simply a way to associated which scenes are on which page before we start cleaning lines and parsing scenes
         id: "",
         page: 0,
         line: 0,
         text: "",
     } ]],
 }
+const docParse = {
+    pageCleaned: [],
+    pageLineCleaned: [[]],
+    pageLineCharCleaned: [[[]]],
+}
+const sceneParse = {
+    scene: [],
+}
+
 
 async function loadPdf(filePath) {
     const dataBuffer = fs.readFileSync(filePath);
@@ -185,30 +195,124 @@ async function loadPdf(filePath) {
     });
 }
 
+async function cleanPageLines() {
+    const specialCharacters = ['--', '!', '?', '@', '%', '...', 'THE END'];
+  
+    function cleanlines(pageLineRaw) {
+      const cleanedPages = [];
+      pageLineRaw.forEach((pageLines, pageIndex) => {
+        const cleanedLines = pageLines.map((line) => {
+          if (typeof line === 'string') {
+            const trimmedLine = line.trim();
+            if (trimmedLine === '' || /^\d+\.$/.test(trimmedLine)) {
+              return null;
+            }
+            return trimmedLine.split('').filter((char) => !specialCharacters.includes(char)).join('');
+          }
+          return line;
+        }).filter(Boolean); // Remove null values (empty or number with a period).
+        cleanedPages.push(cleanedLines);
+        console.log(chalk.bold(`\nPage ${pageIndex + 1}:`));
+        console.log(chalk.dim(`Lines: ${cleanedLines.length}`));
+        console.log(chalk.dim('Cleaned Lines:\n\n'));
+        cleanedLines.forEach((line, lineIndex) => {
+          console.log(chalk.dim(`${lineIndex + 1}: `) + line);
+        });
+        console.log('\n');
+      });
+      return cleanedPages;
+    }
+  
+    docParse.pageLineCleaned = cleanlines(docRaw.pageLineRaw);
+    console.log(chalk.bold(`Number of cleaned pages: ${docParse.pageLineCleaned.length}`));
+}
+function groupScenes(cleanedLines, sceneParse, docRaw) {
+    const headingEnum = [
+      "EXT./INT.", "EXT./INT.", "INT./EXT.", "EXT/INT",
+      "INT/EXT", "INT.", "EXT.", "INT --", "EXT --"
+    ];
+  
+    let currentScene = null;
+  
+    cleanedLines.forEach((line, lineIndex) => {
+      // Check if the line contains any of the allowed headings
+      const hasHeading = headingEnum.some(heading => line.includes(heading));
+  
+      if (hasHeading) {
+        // Start a new scene
+        const sceneText = line.trim();
+        const correspondingScene = docRaw.sceneToPage.find(entry => entry.text === sceneText);
+        const id = correspondingScene ? correspondingScene.id : uuidv4();
+  
+        const scene = {
+          id,
+          page: docRaw.pageRaw.length,
+          line: lineIndex + 1,
+          text: line,
+          content: []
+        };
+        sceneParse.scene.push(scene);
+        docRaw.sceneRaw.push(scene);
+        currentScene = scene;
+      } else if (currentScene) {
+        // Add the line to the current scene's content
+        currentScene.content.push(line);
+      }
+    });
+  
+    // Print the scenes
+    sceneParse.scene.forEach((scene, index) => {
+      console.log(chalk.bold(`\nScene ${index + 1}:`));
+      console.log(chalk.dim(`ID: ${scene.id}`));
+      console.log(chalk.dim(`Page: ${scene.page}`));
+      console.log(chalk.dim(`Line: ${scene.line}`));
+      console.log(chalk.dim(`Text: ${scene.text}`));
+      console.log(chalk.dim('Content:\n'));
+      scene.content.forEach((line, lineNumber) => {
+        console.log(chalk.dim(`${lineNumber + 1}: ${line}`));
+      });
+    });
+  
+    return sceneParse;
+}
+  
+  
+  
+  
+  
+
+  
+  
 
 
 
 
-async function parseScript(filePath) {
+
+  async function parseScript(filePath) {
     // Call 'loadPdf'.
     await loadPdf(filePath);
-
-    // DEBUG Consoles
-    //console.log('DocRaw.pdfRaw: ', docRaw);
-    //console.log('DocRaw.pageRaw: ', docRaw.pageRaw);
-    //console.log('DocRaw.pageLineRaw: ', docRaw.pageLineRaw);
-    //console.log('DocRaw.pageLineCharRaw: ', docRaw.pageLineCharRaw);
-    //console.log('DocRaw.sceneToPage: ', JSON.stringify(docRaw.sceneToPage, null, 2));
-
+    await cleanPageLines();
+    await groupScenes(docParse.pageLineCleaned.flat(), sceneParse, docRaw);
+    //console.log ('docRaw.sceneToPage', JSON.stringify(docRaw.sceneToPage, null, 2));
     console.log(chalk.bold.white("\n------------------------"));
     console.log(chalk.bold.white("     Script Statistics"));
     console.log(chalk.bold.white("------------------------\n"));
 
+    // Raw Statistics
+    console.log(chalk.yellow("Raw Statistics:"));
     console.log(chalk.cyan("Number of Pages: ") + chalk.white(docRaw.pageRaw.length));
     console.log(chalk.cyan("Total Scenes: ") + chalk.white(docRaw.sceneToPage.flat().length));
-    console.log(chalk.cyan("Total Characters: ") + chalk.white(docRaw.pageLineCharRaw.flat(2).length));
-    console.log(chalk.cyan("Total Lines: ") + chalk.white(docRaw.pageLineRaw.flat().length));
-    console.log(chalk.cyan("Total Words: ") + chalk.white(docRaw.pageLineRaw.flat().join(" ").split(/\s+/).length));
+    console.log(chalk.cyan("Total Characters (Raw): ") + chalk.white(docRaw.pageLineCharRaw.flat(2).length));
+    console.log(chalk.cyan("Total Lines (Raw): ") + chalk.white(docRaw.pageLineRaw.flat().length));
+    console.log(chalk.cyan("Total Words (Raw): ") + chalk.white(docRaw.pageLineRaw.flat().join(" ").split(/\s+/).length));
+
+    // Cleaned Statistics
+    console.log(chalk.green("\nCleaned Statistics:"));
+    console.log(chalk.cyan("Number of Pages (Cleaned): ") + chalk.white(docParse.pageLineCleaned.length));
+    console.log(chalk.cyan("Total Scenes (Cleaned): ") + chalk.white(docRaw.sceneToPage.flat().length));
+    console.log(chalk.cyan("Total Characters (Cleaned): ") + chalk.white(docParse.pageLineCharCleaned.flat(2).length));
+    console.log(chalk.cyan("Total Lines (Cleaned): ") + chalk.white(docParse.pageLineCleaned.flat().length));
+    console.log(chalk.cyan("Total Words (Cleaned): ") + chalk.white(docParse.pageLineCleaned.flat().join(" ").split(/\s+/).length));
 
     console.log(chalk.bold.white("\n------------------------\n"));
 }
