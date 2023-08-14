309600685c6cf7d269c0ab5c9533bd4fdd37a6b1
mattrichmo <mattrichmo@github.com>
2023-08-14 11:15:05 -0700

    added UUID for every scene, every line and every char of every line

--- main.mjs
+++ main.mjs
@@ -1,129 +1,139 @@
 import { loadPDF, readPDFToJson } from './components/LoadUtils.mjs';
-import { createCharObjects, combineCharData, parsePageLinesCharData  } from './components/CleanUtils.mjs';
+import { createCharObjects, combineCharData, parsePageLinesCharData } from './components/CleanUtils.mjs';
 import chalk from 'chalk';
-
+import { v4 as uuidv4 } from 'uuid';
 
 const docRaw = {
-    pdfRaw: {},
-    pagesJSONRaw: [],
-    pdfRawText: [{}],
-    pagesRaw: [{
-        pageIndex: 0,
-        pageID: "", 
-        pageRawText: "",
-        pageLines: [[""]],
-        pageLineChars: [[['']]]
-        }],
-    rawTextJSONArray : [{}],
-    combinedChar: [{}],
-
+  pdfRaw: {},
+  pagesJSONRaw: [],
+  pdfRawText: [{}],
+  pagesRaw: [
+    {
+      pageIndex: 0,
+      pageID: '',
+      pageRawText: '',
+      pageLines: [['']],
+      pageLineChars: [[['']]],
+    },
+  ],
+  rawTextJSONArray: [{}],
+  combinedChar: [{}],
 };
+
 const sceneParse = {
-
-    scenes: [{
-        titleRaw: "", // the line the scene title is on
-        bodyRaw: "", // single strong of all the text in the scene
-        body: [],
-        lines: [{
-            lineText: "",
-            lineNumber: null,
-        }],
-        lineChars: [{
-            text: "",
-            x: null,
-            y: null,
-            w: null,
-            clr: null,
-            sw: null,
-        }],
-    }],
+  scenes: [
+    {
+      titleRaw: '',
+      bodyRaw: '',
+      body: [],
+      lines: [
+        {
+          lineText: '',
+          lineNumber: null,
+        },
+      ],
+      lineChars: [
+        {
+          text: '',
+          x: null,
+          y: null,
+          w: null,
+          clr: null,
+          sw: null,
+        },
+      ],
+    },
+  ],
 };
 
+const parseScenes = (docRaw, sceneParse) => {
+  const headingEnum = [
+    'EXT./INT.',
+    'EXT./INT.',
+    'INT./EXT.',
+    'EXT/INT',
+    'INT/EXT',
+    'INT.',
+    'EXT.',
+    'INT --',
+    'EXT --',
+  ];
 
+  const charArray = docRaw.combinedChar.map((charObj) => charObj.text);
+  const scenes = charArray
+    .join('')
+    .split(new RegExp(headingEnum.join('|'), 'g'))
+    .map((scene, sceneIndex) => {
+      const sceneId = uuidv4();
+      const lines = scene
+        .split(/\r?\n/)
+        .map((line, lineNumber) => {
+          const lineId = uuidv4();
+          const lineChars = line.split('').map((charItem) => {
+            const charObj = docRaw.combinedChar.find((obj) => obj.text === charItem);
+            return charObj ? { ...charObj, id: uuidv4(), lineId: lineId, sceneId: sceneId } : { text: charItem, id: uuidv4(), lineId: lineId, sceneId: sceneId };
+          });
 
-
-const parseScenes = async (docRaw, sceneParse) => {
-    const headingEnum = [
-        "EXT./INT.", "EXT./INT.", "INT./EXT.", "EXT/INT",
-        "INT/EXT", "INT.", "EXT.", "INT --", "EXT --"
-    ];
-
-    const charArray = docRaw.combinedChar.map(charObj => charObj.text);
-    //join the characters then split if a line includes any of the headingEnum
-    const scenes = charArray.join('').split(new RegExp(headingEnum.join('|'), 'g'));
-    sceneParse.scenes = scenes.map((scene, sceneIndex) => {
-        const lines = scene.split(/\r?\n/);
-        const lineChars = lines.map((line, lineNumber) => {
-            const lineChars = line.split('').map((charItem) => { // Change 'char' to 'charItem' here
-                const charObj = docRaw.combinedChar.find(obj => obj.text === charItem);
-                return charObj ? charObj : { text: charItem };
-            });
-            return {
-                lineNumber: lineNumber + 1,
-                lineText: line,
-                lineChars: lineChars,
-            };
-        });
-        return {
-            titleRaw: lines[0],
-            bodyRaw: scene,
-            lines: lines,
+          return {
+            id: lineId,
+            sceneId: sceneId,
+            lineNumber: lineNumber + 1,
+            lineText: line,
             lineChars: lineChars,
-        };
-    });
-};
-const cleanScenes = (sceneParse) => {
-    const specialCharacters = ['%', '!', '#'];
-    
-    sceneParse.scenes.forEach((scene) => {
-        const cleanedLines = [];
-
-        scene.lineChars.forEach((lineObj, index) => {
-            const lineText = scene.lines[index];
-
-            // Check if line is not empty, is not a number possibly followed by a period or parenthesis, and does not start with a special character
-            if (lineText.trim() !== '' && !lineText.match(/^\d+[.)]?$/) && !specialCharacters.some(char => lineText.startsWith(char))) {
-                cleanedLines.push({
-                    lineText: lineText,
-                    lineChars: lineObj.lineChars,
-                });
-            }
+          };
         });
 
-        // Update lines and lineChars in the scene object
-        scene.lines = cleanedLines.map(lineObj => lineObj.lineText);
-        scene.lineChars = cleanedLines.map(lineObj => ({ lineText: lineObj.lineText, lineChars: lineObj.lineChars })); 
+      return {
+        id: sceneId,
+        titleRaw: lines[0].lineText,
+        bodyRaw: scene,
+        lines: lines,
+      };
     });
+
+  sceneParse.scenes = scenes;
 };
 
+const cleanScenes = (sceneParse) => {
+  const specialCharacters = ['%', '!', '#'];
 
+  sceneParse.scenes.forEach((scene) => {
+    const cleanedLines = [];
+
+    scene.lines.forEach((lineObj, index) => {
+      const lineText = lineObj.lineText;
+
+      // Check if line is not empty, is not a number possibly followed by a period or parenthesis, and does not start with a special character
+      if (
+        lineText.trim() !== '' &&
+        !lineText.match(/^\d+[.)]?$/) &&
+        !specialCharacters.some((char) => lineText.startsWith(char))
+      ) {
+        cleanedLines.push(lineObj);
+      }
+    });
+
+    scene.lines = cleanedLines;
+  });
+};
 
 const initialLoad = async () => {
-    await loadPDF(docRaw);
-    await readPDFToJson(docRaw);
-    await createCharObjects(docRaw);
-    await combineCharData(docRaw);
-    //await parsePagesandLines(docRaw);
-    await parsePageLinesCharData(docRaw);
-    await parseScenes(docRaw, sceneParse);
-    await cleanScenes(sceneParse);
-    //console.log('sceneParse', JSON.stringify(sceneParse, null, 2));
-    console.log('sceneParse', sceneParse);
-    sceneParse.scenes.forEach((scene, sceneIndex) => {
-        console.log(chalk.bold(`Scene ${sceneIndex + 1}: ${chalk.underline(scene.titleRaw)}`));
-        scene.lineChars.forEach((line, lineIndex) => {
-            console.log(chalk.dim(`Line ${lineIndex + 1}:`), chalk.white(line.lineText));
-            console.log(chalk.gray.dim('cc:'), line.lineChars.length);
-        });
+  await loadPDF(docRaw);
+  await readPDFToJson(docRaw);
+  await createCharObjects(docRaw);
+  await combineCharData(docRaw);
+  await parsePageLinesCharData(docRaw);
+  parseScenes(docRaw, sceneParse);
+  cleanScenes(sceneParse);
+
+  console.log('sceneParse', sceneParse);
+  sceneParse.scenes.forEach((scene, sceneIndex) => {
+    console.log(chalk.bold(`Scene ${sceneIndex + 1}: ${chalk.underline(scene.titleRaw)}`));
+    scene.lines.forEach((line, lineIndex) => {
+      console.log(chalk.dim(`${lineIndex + 1} |`), chalk.white(line.lineText));
+      //console.log(chalk.gray.dim('cc:'), line.lineChars.length, chalk.gray.dim('Scene ID:'), scene.id, chalk.gray.dim('Line ID:'), line.id);
     });
-
-
+  });
 };
 
-
-
-
 initialLoad();
-
-  
-  
