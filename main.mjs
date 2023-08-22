d0fe942c1a8548cf8441aec60a04aff80f86a8ca
mattrichmo <mattrichmo@github.com>
2023-08-22 16:37:58 -0700

    cleaning code

--- main.mjs
+++ main.mjs
@@ -2,16 +2,15 @@ import { loadPDF, readPDFToJson } from './components/LoadUtils.mjs';
 import { createCharObjects, combineCharData, parsePageLinesCharData, parseLinesCharData } from './components/CleanUtils.mjs';
 import { parseScenes, cleanScenes, updateSceneHeaders, extractScriptCharacters } from './components/ParseUtils.mjs';
 import { setElementType, setDualDialogue, parseElements } from './components/ElementHelper.mjs';
+import { saveToDatabase } from './components/DB.mjs';
 import chalk from 'chalk';
 import express from 'express';
-import sqlite3 from 'sqlite3';
 
-const db = new sqlite3.Database('mydb.sqlite3');
 // !Important. When you first run npm i, go to node_modules/pdf-parse/index.js and delete all the code in between the debugger tag. For some reason it overrides this file locastion and will say no file exists and throw an error
 
 // ************************************************************
 
-export const PDF_FILE = './scripts/kosi.pdf'; // Define the PDF_FILE constant here
+export const PDF_FILE = './scripts/bb.pdf'; // Define the PDF_FILE constant here
 
 // ************************************************************
 
@@ -52,7 +51,23 @@ const sceneParse = {
     sceneIndex: 0,
     sceneTitle: '',
     bodyRaw: '',
-    body: [],
+    body: [], // Required: Body of the scene (array)
+    animals: [], // Optional: List of animals in the scene (array)
+    authors: [], // Optional: List of author IDs (array)
+    cast: [], // Optional: List of cast members (array)
+    contributors: [], // Optional: List of contributor IDs (array)
+    extra: [], // Optional: List of extra elements in the scene (array)
+    id: "", // Optional: Unique identifier for the scene (string)
+    locations: [], // Optional: List of locations in the scene (array)
+    moods: [], // Optional: List of moods in the scene (array)
+    props: [{
+      propItem: '',
+    }], // Optional: List of props in the scene (array)
+    sfx: [], // Optional: List of sound effects in the scene (array)
+    sounds: [], // Optional: List of sounds in the scene (array)
+    tags: [], // Optional: List of tags for the scene (array)
+    vfx: [], // Optional: List of visual effects in the scene (array)
+    wardrobe: [], // Optional: List of wardrobe items in the scene (array)
     lines: [{
       lineID: "uuidv4()",
         lineText: '',
@@ -98,8 +113,8 @@ const sceneParse = {
         sceneLineIndex: 0,
       },
       type: '', // type of action, character, dialogue, general, parenthetical, shot, transition
+      dual: 0, // boolean value. is 1 if multiple dialogue are present in the lines before or after 
       elementRawLines : [],
-      dual: 0 // boolean value. is 1 if multiple dialogue are present in the lines before or after 
     }],  
     },
   ],
@@ -124,98 +139,21 @@ const startServer = async (sceneParse) => {
     console.log('Server is running on port 3000. Link: http://localhost:3000/api/sceneParse');
   });
 };
-const saveToDatabase = async (sceneParse, docRaw) => {
-  try {
-    const db = new sqlite3.Database('scenes.sqlite3');
-    console.log('Connected to the database.');
 
-    await db.run(`
-      CREATE TABLE IF NOT EXISTS scenes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        scene_title TEXT,
-        scene_body TEXT,
-        scene_elements TEXT,
-        scene_number INTEGER
-      )
-    `);
-    console.log('Table "scenes" created successfully.');
-
-    await db.run(`
-      CREATE TABLE IF NOT EXISTS elements (
-        id TEXT PRIMARY KEY,
-        parent_scene_id TEXT,
-        parent_scene_index INTEGER,
-        parent_scene_title TEXT,
-        parent_scene_line_index INTEGER,
-        type TEXT,
-        element_raw_lines TEXT,
-        dual INTEGER
-      )
-    `);
-    console.log('Table "elements" created successfully.');
-
-    await db.run(`
-      CREATE TABLE IF NOT EXISTS doc_raw (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        pdf_raw TEXT,
-        pages_json_raw TEXT,
-        pdf_raw_text TEXT,
-        pages_raw TEXT,
-        raw_text_json_array TEXT,
-        combined_char TEXT,
-        combined_char_lines TEXT
-      )
-    `);
-    console.log('Table "doc_raw" created successfully.');
-
-    const stmtScenes = db.prepare('INSERT INTO scenes (scene_title, scene_body, scene_elements, scene_number) VALUES (?, ?, ?, ?)');
-    const stmtElements = db.prepare('INSERT INTO elements (id, parent_scene_id, parent_scene_index, parent_scene_title, parent_scene_line_index, type, element_raw_lines, dual) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
-    const stmtDocRaw = db.prepare('INSERT INTO doc_raw (pdf_raw, pages_json_raw, pdf_raw_text, pages_raw, raw_text_json_array, combined_char, combined_char_lines) VALUES (?, ?, ?, ?, ?, ?, ?)');
-
-    sceneParse.scenes.forEach((scene) => {
-      stmtScenes.run(scene.sceneTitle, JSON.stringify(scene.body), JSON.stringify(scene.elements), scene.sceneIndex + 1);
-      console.log('Scene data inserted successfully.');
-
-      scene.elements.forEach((element) => {
-        stmtElements.run(
-          element.elementID,
-          scene.sceneID,
-          scene.sceneIndex,
-          scene.sceneTitle,
-          element.parentScene.sceneLineIndex,
-          element.type,
-          JSON.stringify(element.elementRawLines),
-          element.dual
-        );
-        console.log('Element data inserted successfully.');
-      });
+const sortElementType = (sceneParse) => {
+  sceneParse.scenes.forEach((scene) => {
+    const elements = scene.elements;
+    elements.forEach((element) => {
+      if (element.type === 'prop') {
+        const propItems = element.elementRawLines.map((line) => line.lineText.trim());
+        scene.props = propItems.map((propItem) => ({ propItem }));
+      }
+      if (element.type === 'dialogue') {
+        const dialogueItems = element.elementRawLines.map((line) => line.lineText.trim());
+        scene.cast = dialogueItems.map((dialogueItem) => ({ dialogueItem }));
+      }
     });
-
-    stmtScenes.finalize();
-    console.log('Finalized scenes statement.');
-
-    stmtElements.finalize();
-    console.log('Finalized elements statement.');
-
-    stmtDocRaw.run(
-      JSON.stringify(docRaw.pdfRaw),
-      JSON.stringify(docRaw.pagesJSONRaw),
-      JSON.stringify(docRaw.pdfRawText),
-      JSON.stringify(docRaw.pagesRaw),
-      JSON.stringify(docRaw.rawTextJSONArray),
-      JSON.stringify(docRaw.combinedChar),
-      JSON.stringify(docRaw.combinedCharLines)
-    );
-    console.log('Doc raw data inserted successfully.');
-
-    stmtDocRaw.finalize();
-    console.log('Finalized doc raw statement.');
-
-    db.close();
-    console.log('Closed the database connection.');
-  } catch (err) {
-    console.error('Error:', err.message);
-  }
+  });
 };
 
 //Group Functions 
@@ -237,6 +175,8 @@ const main = async (docRaw, sceneParse, scriptCharacters) => {
   await parseScenesToObject(docRaw, sceneParse);
   await extractScriptCharacters(sceneParse, scriptCharacters); 
   await parseElements(sceneParse);
+  await sortElementType(sceneParse);
+
   console.log('screens', JSON.stringify(sceneParse));
 
   console.log(chalk.bold.blue('Parsed scene information:\n'));
@@ -250,6 +190,11 @@ const main = async (docRaw, sceneParse, scriptCharacters) => {
     console.log(chalk.cyan('Scene Setting:'), scene.heading.setting);
     console.log(chalk.cyan('Scene Sequence:'), scene.heading.sequence);
     console.log(chalk.cyan('Prod Scene #'), scene.heading.prodSceneNum);
+    console.log(chalk.blue('Props:'))
+    scene.props.forEach((prop, propIndex) => {
+      console.log(chalk.cyan(`Prop ${propIndex + 1}:`), prop.propItem);
+    });
+
 
     console.log(chalk.yellow('\nScene Lines:\n'));
     scene.linesCleaned.forEach((line, lineIndex) => {
