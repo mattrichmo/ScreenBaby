0ad85d6be8e3ed046252fd85490b1af90415ee65
mattrichmo <matt.richmond3@gmail.com>
2023-08-23 10:34:53 -0700

    scene data tagging and extraction

--- components/ElementHelper.mjs
+++ components/ElementHelper.mjs
@@ -4,21 +4,21 @@ export const setElementType = (element) => {
   const firstLine = element.elementRawLines[0].lineText.trim();
 
   if (firstLine.match(/^(FADE OUT|FADE IN|FADE TO BLACK|FADE TO WHITE|CUT TO|CUT IN|CUT TO BLACK|CUT TO WHITE|DISSOLVE TO|IRIS OUT|IRIS IN|WIPE TO|SMASH CUT TO|MATCH CUT TO|JUMP CUT TO|CUTAWAY TO|CROSSFADE TO|FADE THROUGH TO|FLASH TO|FREEZE FRAME|FADE TO SILENCE|TIME CUT TO|REVERSE CUT TO|CONTINUOUS)/)) {
-    element.type = 'transition';
+    element.groupType = 'transition';
   } else if (firstLine.match(/^[A-Z]+$/)) {
     if (firstLine.match(/[A-Z]+\!$/)) {
-      element.type = 'action';
+      element.groupType = 'action';
     } else {
-      element.type = 'dialogue';
+      element.groupType = 'dialogue';
     }
   } else if (firstLine.match(/[a-z]+ [A-Z][a-z]*/)) {
-    element.type = 'prop';
+    element.groupType = 'prop';
   } else if (firstLine.match(/^\s*\(\w+\)\s*$/)) {
-    element.type = 'parenthesis';
+    element.groupType = 'parenthesis';
   } else if (firstLine.match(/^(PAN|TILT|ZOOM|DOLLY|TRACK|CRANE|STEADICAM|HANDHELD)(\s+(UP|DOWN|LEFT|RIGHT|IN|OUT|FORWARD|BACKWARD|UPWARD|DOWNWARD|LEFTWARD|RIGHTWARD|INWARD|OUTWARD|FORWARDS|BACKWARDS|UPWARDS|DOWNWARDS|LEFTWARDS|RIGHTWARDS|INWARDS|OUTWARDS|CLOSE ON))?$/)) {
-    element.type = 'camera';
+    element.groupType = 'camera';
   } else if (firstLine.match(/\b[A-Z]+ING\b$/)) { // Check if the word ends with ING (capitalized)
-    element.type = 'action'; // Set the type of the element to 'action'
+    element.groupType = 'action'; // Set the type of the element to 'action'
   }
 };
 
@@ -28,7 +28,7 @@ export const setDualDialogue = (elements) => {
   let consecutiveDialogueCount = 0;
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
-    if (element.type === 'dialogue') {
+    if (element.groupType === 'dialogue') {
       consecutiveDialogueCount++;
       if (consecutiveDialogueCount > 1) {
         element.dual = 1;
@@ -39,7 +39,7 @@ export const setDualDialogue = (elements) => {
   }
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
-    if (element.type === 'dialogue' && element.dual === 1) {
+    if (element.groupType === 'dialogue' && element.dual === 1) {
       let j = i - 1;
       while (j >= 0 && elements[j].type === 'dialogue') {
         elements[j].dual = 1;
@@ -60,7 +60,7 @@ export const parseElements = async (sceneParse) => {
 
     let currentElement = null;
 
-    scene.linesCleaned.forEach((line) => {
+    scene.linesCleaned.forEach((line, lineIndex) => {
       const matchResult = line.lineText.match(/\b(?![A-Z](?!-?[A-Z])\b)(?![A-Z]-[A-Z](?!-[A-Z])\b)[A-Z][A-Z-]{0,}(?<!-)(?:\s+[A-Z][A-Z-]{1,}(?<!-))*\b/g);
       
       if (matchResult) {
@@ -78,11 +78,15 @@ export const parseElements = async (sceneParse) => {
             sceneTitle: scene.heading.headingString,
             sceneLineIndex: scene.linesCleaned.indexOf(line),
           },
-          item: foundItem, // Store the found item in the element
-          type: '',
-          characters: [],
-          elementRawLines: [],
+          groupType: '',
           dual: 0,
+          elementRawLines: [],
+          item: foundItem.map((item) => ({
+            name: item,
+            id: uuidv4(),
+            type: '',
+            sceneLocation: lineIndex,
+          })),
         };
       }
 
--- components/PrettyLog.mjs
+++ components/PrettyLog.mjs
@@ -1,56 +1,75 @@
 import chalk from 'chalk';
 
 export const prettyLog = (docRaw, sceneParse) => {
-    console.log('screens', JSON.stringify(sceneParse));
-  
-    console.log(chalk.bold.blue('Parsed scene information:\n'));
-  
-    sceneParse.scenes.forEach((scene, sceneIndex) => {
-      const hiddenLinesCount = scene.lines.length - scene.linesCleaned.length;
-  
-      console.log(chalk.bold.green(`\nSC${sceneIndex + 1}: ${chalk.underline(scene.heading.headingString)}`));
-      console.log();
-      console.log(chalk.cyan('Scene Context:'), scene.heading.context);
-      console.log(chalk.cyan('Scene Setting:'), scene.heading.setting);
-      console.log(chalk.cyan('Scene Sequence:'), scene.heading.sequence);
-      console.log(chalk.cyan('Prod Scene #'), scene.heading.prodSceneNum);
-      console.log(chalk.blue('Props:'))
-      scene.props.forEach((prop, propIndex) => {
-        console.log(chalk.cyan(`Prop ${propIndex + 1}:`), prop.propItem);
+  console.log('screens', JSON.stringify(sceneParse));
+
+  console.log(chalk.bold.blue('Parsed scene information:\n'));
+
+  sceneParse.scenes.forEach((scene, sceneIndex) => {
+    const hiddenLinesCount = scene.lines.length - scene.linesCleaned.length;
+
+    console.log(chalk.bold.green(`\nSC${sceneIndex + 1}: ${chalk.underline(scene.heading.headingString)}`));
+    console.log();
+    console.log(chalk.cyan('Scene Context:'), scene.heading.context);
+    console.log(chalk.cyan('Scene Setting:'), scene.heading.setting);
+    console.log(chalk.cyan('Scene Sequence:'), scene.heading.sequence);
+    console.log(chalk.cyan('Prod Scene #'), scene.heading.prodSceneNum);
+
+    // Display the total number of dialogue lines for the scene
+    let totalDialogueLines = 0;
+    scene.dialogueLines.forEach((dialogueLine) => {
+      totalDialogueLines += dialogueLine.lines.length;
+    });
+    console.log(chalk.cyan(`Total Dialogue Lines: ${totalDialogueLines}`));
+
+    // Display the total number of transitions for the scene
+    const totalTransitions = scene.transitions.length;
+    console.log(chalk.cyan(`Total Transitions: ${totalTransitions}`));
+
+    console.log('-----------------------------------');
+    
+    console.log(chalk.blue('Props:'))
+    scene.props.forEach((prop, propIndex) => {
+      console.log(chalk.cyan(`Prop ${propIndex + 1}:`), prop.propItem);
+    });
+    console.log(chalk.bold.yellow(`Characters in SC${sceneIndex + 1}:\n`));
+    scene.cast.forEach((cast, castIndex) => {
+      console.log(chalk.yellow(`Character ${castIndex + 1}: ${cast.characterName} (${cast.characterLineCount} lines)`));
+    });
+
+
+    console.log(chalk.yellow('\nScene Lines:\n'));
+    scene.linesCleaned.forEach((line, lineIndex) => {
+      console.log(chalk.dim(`${lineIndex + 1} |`), chalk.white(line.lineText));
+    });
+
+    if (hiddenLinesCount > 0) {
+      console.log(chalk.red('\n Cleaned Lines:'), hiddenLinesCount);
+    }
+
+    console.log(chalk.dim.gray('\n' + '-'.repeat(60) + '\n'));
+
+    console.log(chalk.bold.magenta(`Elements in SC${sceneIndex + 1}:\n`));
+    const elements = scene.elements; // Access the elements array for the current scene
+    elements.forEach((element, elementIndex) => {
+      console.log(chalk.magenta(`Element ${elementIndex + 1}:`));
+      console.log(chalk.cyan('Element ID:'), element.elementID);
+      console.log(chalk.cyan('Parent Scene ID:'), element.parentScene.sceneID);
+      console.log(chalk.cyan('Parent Scene Title:'), element.parentScene.sceneTitle);
+      console.log(chalk.cyan('Group Type:'), element.groupType);
+      console.log(chalk.cyan('Dual:'), element.dual);
+      
+      // Display the element item name and scene location
+      element.item.forEach((item) => {
+        console.log(chalk.dim(`scene line`, item.sceneLocation, '|'),chalk.yellow('e.'), item.name, );
       });
-      console.log(chalk.bold.yellow(`Characters in SC${sceneIndex + 1}:\n`));
-      scene.characters.forEach((character, characterIndex) => {
-        console.log(chalk.yellow(`Character ${characterIndex + 1}: ${character.characterName} (${character.characterLineCount} lines)`));
-      });
-  
-  
-      console.log(chalk.yellow('\nScene Lines:\n'));
-      scene.linesCleaned.forEach((line, lineIndex) => {
+
+      console.log(chalk.yellow('Element Raw Lines:'));
+      element.elementRawLines.forEach((line, lineIndex) => {
         console.log(chalk.dim(`${lineIndex + 1} |`), chalk.white(line.lineText));
       });
-  
-      if (hiddenLinesCount > 0) {
-        console.log(chalk.red('\n Cleaned Lines:'), hiddenLinesCount);
-      }
-  
       console.log(chalk.dim.gray('\n' + '-'.repeat(60) + '\n'));
-  
-      console.log(chalk.bold.magenta(`Elements in SC${sceneIndex + 1}:\n`));
-      const elements = scene.elements; // Access the elements array for the current scene
-      elements.forEach((element, elementIndex) => {
-        console.log(chalk.magenta(`Element ${elementIndex + 1}:`));
-        console.log(chalk.cyan('Element ID:'), element.elementID);
-        console.log(chalk.cyan('Parent Scene ID:'), element.parentScene.sceneID);
-        console.log(chalk.cyan('Parent Scene Title:'), element.parentScene.sceneTitle);
-        console.log(chalk.cyan('Type:'), element.type);
-        console.log(chalk.cyan('Dual:'), element.dual);
-        console.log(chalk.yellow('Element Item:'), element.item); // Display the element item
-        console.log(chalk.yellow('Element Raw Lines:'));
-        element.elementRawLines.forEach((line, lineIndex) => {
-          console.log(chalk.dim(`${lineIndex + 1} |`), chalk.white(line.lineText));
-        });
-        console.log(chalk.dim.gray('\n' + '-'.repeat(60) + '\n'));
-      });
-      console.log(chalk.dim.gray('-'.repeat(60) + '\n'));
     });
-  };
+    console.log(chalk.dim.gray('-'.repeat(60) + '\n'));
+  });
+};
